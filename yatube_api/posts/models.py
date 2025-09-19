e1a8bd7f63104ae65bd321e0e163ddfb865d2b7c
mrSoftIce <mr.softice@gmail.com>
2025-09-19 23:29:31 +0400

    Models update Follow model added

--- yatube_api/posts/models.py
+++ yatube_api/posts/models.py
@@ -4,8 +4,17 @@ from django.db import models
 User = get_user_model()
 
 
+class Group(models.Model):
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=50, unique=True)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.title
+
+
 class Post(models.Model):
-    text = models.TextField()
+    text = models.TextField('Текст публикации')
     pub_date = models.DateTimeField('Дата публикации', auto_now_add=True)
     author = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name='posts')
@@ -13,7 +22,11 @@ class Post(models.Model):
         upload_to='posts/', null=True, blank=True)
 
     def __str__(self):
-        return self.text
+        return (
+            f'{self.text[:128]}\n'
+            f'Author: {self.author}\n'
+            f'Published: {self.pub_date.strftime("%Y-%m-%d %H:%M:%S")}'
+        )
 
 
 class Comment(models.Model):
@@ -21,6 +34,13 @@ class Comment(models.Model):
         User, on_delete=models.CASCADE, related_name='comments')
     post = models.ForeignKey(
         Post, on_delete=models.CASCADE, related_name='comments')
-    text = models.TextField()
+    text = models.TextField('текст комментария')
     created = models.DateTimeField(
         'Дата добавления', auto_now_add=True, db_index=True)
+
+
+class Follow(models.Model):
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='follower')
+    following = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='followings')
