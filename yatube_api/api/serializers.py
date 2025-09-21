73accf1366f3ef317c47196c89c70c1f8b095b68
mrSoftIce <mr.softice@gmail.com>
2025-09-21 19:07:52 +0400

    serializers and views

--- yatube_api/api/serializers.py
+++ yatube_api/api/serializers.py
@@ -1,7 +1,10 @@
+from django.contrib.auth import get_user_model
 from rest_framework import serializers
 from rest_framework.serializers import ModelSerializer
 
-from posts.models import Comment, Group, Post
+from posts.models import Comment, Follow, Group, Post
+
+User = get_user_model()
 
 
 class GroupSerializer(ModelSerializer):
@@ -12,19 +15,42 @@ class GroupSerializer(ModelSerializer):
 
 
 class PostSerializer(ModelSerializer):
-    author = serializers.CharField(source='author.username')
+    author = serializers.CharField(source='author.username', read_only=True)
 
     class Meta:
-        fields = '__all__'
         model = Post
+        fields = '__all__'
         read_only_fields = ('author', 'pub_date',)
 
 
 class CommentSerializer(ModelSerializer):
-    author = serializers.SlugRelatedField(
-        source='author.username', slug_field='username')
+    author = serializers.ReadOnlyField(source='author.username',)
 
     class Meta:
-        fields = '__all__'
         model = Comment
+        fields = '__all__'
         read_only_fields = ('author', 'created', 'post')
+
+
+class FollowSerializer(ModelSerializer):
+    user = serializers.CharField(source='user.username', read_only=True)
+    following = serializers.CharField()
+
+    class Meta:
+        model = Follow
+        fields = ('user', 'following',)
+        read_only_fields = ('user',)
+        validators = [
+            serializers.UniqueTogetherValidator(
+                queryset=Follow.objects.all(),
+                fields=('user', 'following'),
+                message='Вы уже подписаны на этого пользователя'
+            )
+        ]
+
+    def validate_following(self, value):
+        try:
+            User.objects.get(username=value)
+        except User.DoesNotExist:
+            raise serializers.ValidationError('Пользователь не найден.')
+        return value
--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,1 +1,122 @@
-# TODO:  Напишите свой вариант
+from django.shortcuts import get_object_or_404
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import filters, mixins, status
+from rest_framework.pagination import LimitOffsetPagination
+from rest_framework.permissions import (
+    IsAuthenticated,
+    IsAuthenticatedOrReadOnly,
+)
+from rest_framework.response import Response
+from rest_framework.viewsets import (
+    GenericViewSet,
+    ModelViewSet,
+    ReadOnlyModelViewSet,
+)
+from rest_framework_simplejwt.exceptions import InvalidToken, TokenError
+from rest_framework_simplejwt.views import TokenRefreshView, TokenVerifyView
+
+from api.pagination import CustomPostPagination
+from api.permissions import IsAuthorOrReadOnly
+from api.serializers import (
+    CommentSerializer,
+    FollowSerializer,
+    GroupSerializer,
+    PostSerializer,
+)
+from posts.models import Follow, Group, Post, User
+
+
+class CustomTokenRefreshView(TokenRefreshView):
+    def post(self, request, *args, **kwargs):
+        try:
+            return super().post(request, *args, **kwargs)
+        except (InvalidToken, TokenError):
+            return Response(
+                {
+                    "code": "token_not_valid",
+                    "detail": "Token is invalid or expired"
+                },
+                status=status.HTTP_401_UNAUTHORIZED
+            )
+
+
+class CustomTokenVerifyView(TokenVerifyView):
+    def post(self, request, *args, **kwargs):
+        try:
+            return super().post(request, *args, **kwargs)
+        except (InvalidToken, TokenError):
+            return Response(
+                {
+                    "code": "token_not_valid",
+                    "detail": "Token is invalid or expired"
+                },
+                status=status.HTTP_401_UNAUTHORIZED
+            )
+
+
+class GroupViewSet(ReadOnlyModelViewSet):
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+
+
+class PostViewSet(ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    pagination_class = CustomPostPagination
+    filter_backends = (DjangoFilterBackend, filters.SearchFilter)
+    filterset_fields = ('author__username', 'group', )
+    search_fields = ('text',)
+    permission_classes = [IsAuthorOrReadOnly, IsAuthenticatedOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+
+class CommentViewSet(ModelViewSet):
+    serializer_class = CommentSerializer
+    permission_classes = [IsAuthorOrReadOnly, IsAuthenticatedOrReadOnly]
+
+    def get_post_or_404(self):
+        return get_object_or_404(Post, pk=self.kwargs['post_id'])
+
+    def get_queryset(self):
+        return self.get_post_or_404().comments.all()
+
+    def perform_create(self, serializer):
+        serializer.save(
+            author=self.request.user,
+            post=self.get_post_or_404()
+        )
+
+
+class FollowViewSet(mixins.CreateModelMixin,
+                    mixins.ListModelMixin,
+                    GenericViewSet):
+    queryset = Follow.objects.all()
+    serializer_class = FollowSerializer
+    permission_classes = [IsAuthenticated]
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('following__username',)
+
+    def get_queryset(self):
+        return Follow.objects.filter(user=self.request.user)
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        following_username = serializer.validated_data['following']
+        if not following_username:
+            return Response(
+                '{ "following": ["Обязательное поле."] }',
+                status=status.HTTP_400_BAD_REQUEST
+            )
+        if following_username == request.user.username:
+            return Response(
+                {'following': ['Нельзя подписаться на самого себя.']},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+        following = get_object_or_404(User, username=following_username)
+
+        follow = Follow.objects.create(user=request.user, following=following)
+        serializer = self.get_serializer(follow)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
