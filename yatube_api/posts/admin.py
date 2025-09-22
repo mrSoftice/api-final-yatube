3fa82bd5995f46259ca83970982bf72850b749df
mrSoftIce <mr.softice@gmail.com>
2025-09-22 12:10:14 +0400

    v0.2 PEP8 online tests

--- yatube_api/posts/admin.py
+++ yatube_api/posts/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Comment, Group, Post
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'text', 'pub_date', 'author')
+    search_fields = ('text',)
+    list_filter = ('author', 'group')
+    empty_value_display = '-пусто-'
+
+
+admin.site.register(Group)
+admin.site.register(Comment)
